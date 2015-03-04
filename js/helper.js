6ec710220f68651a2449f64a69ab8544fc80ccf4
Jim Curran <jimcee@gmail.com>
2015-03-04 00:09:37 -0800

    Removed newer of 2 jQuery libraries, and styling

--- js/helper.js
+++ js/helper.js
@@ -354,52 +354,7 @@
     };
 
     /**
-     * iOS Startup Image helper
+     * iOS Startup Image helper CUT JC
      */
-
-    MBP.startupImage = function() {
-        var portrait;
-        var landscape;
-        var pixelRatio;
-        var head;
-        var link1;
-        var link2;
-
-        pixelRatio = window.devicePixelRatio;
-        head = document.getElementsByTagName('head')[0];
-
-        if (navigator.platform === 'iPad') {
-            portrait = pixelRatio === 2 ? 'img/startup/startup-tablet-portrait-retina.png' : 'img/startup/startup-tablet-portrait.png';
-            landscape = pixelRatio === 2 ? 'img/startup/startup-tablet-landscape-retina.png' : 'img/startup/startup-tablet-landscape.png';
-
-            link1 = document.createElement('link');
-            link1.setAttribute('rel', 'apple-touch-startup-image');
-            link1.setAttribute('media', 'screen and (orientation: portrait)');
-            link1.setAttribute('href', portrait);
-            head.appendChild(link1);
-
-            link2 = document.createElement('link');
-            link2.setAttribute('rel', 'apple-touch-startup-image');
-            link2.setAttribute('media', 'screen and (orientation: landscape)');
-            link2.setAttribute('href', landscape);
-            head.appendChild(link2);
-        } else {
-            portrait = pixelRatio === 2 ? "img/startup/startup-retina.png" : "img/startup/startup.png";
-            portrait = screen.height === 568 ? "img/startup/startup-retina-4in.png" : portrait;
-            link1 = document.createElement('link');
-            link1.setAttribute('rel', 'apple-touch-startup-image');
-            link1.setAttribute('href', portrait);
-            head.appendChild(link1);
-        }
-
-        //hack to fix letterboxed full screen web apps on 4" iPhone / iPod with iOS 6
-        if (navigator.platform.match(/iPhone|iPod/i) && (screen.height === 568) && navigator.userAgent.match(/\bOS 6_/)) {
-            if (MBP.viewportmeta) {
-                MBP.viewportmeta.content = MBP.viewportmeta.content
-                    .replace(/\bwidth\s*=\s*320\b/, 'width=320.1')
-                    .replace(/\bwidth\s*=\s*device-width\b/, '');
-            }
-        }
-    };
 
 })(document);
